d326051efa18b78a2eae9548f7945f244074cc15
Domedriver <mlaman@gmail.com>
2019-03-27 14:49:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const getNotes = require('./notes.js')
 var notes = getNotes()
 
 // console.log the return value of the function getNotes
+// just wanted to add another comment here
 console.log(notes)
