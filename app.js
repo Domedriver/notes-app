8df8df0951e3db1ccf58f9f159ae98fb60f81311
Domedriver <mlaman@gmail.com>
2019-03-27 14:54:06 -0400

    added another comment'

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ var notes = getNotes()
 
 // console.log the return value of the function getNotes
 // just wanted to add another comment here
+// now I'm going to add one more comment
 console.log(notes)
