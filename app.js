7d8754d2bc048b830ff88b68cf350ab7d8dfde7d
Domedriver <mlaman@gmail.com>
2019-03-28 14:27:08 -0400

    re-added notes.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const chalk = require('chalk')
-//const getNotes = require('./notes.js')
+const getNotes = require('./notes.js')
 
-//var notes = getNotes()
+var notes = getNotes()
 
-//console.log(notes)
+console.log(notes)
 
-console.log(chalk.black.bgGreen.bold('Success!'))
+console.log(chalk.black.bgRed.bold.underline('Error'))
 
 
