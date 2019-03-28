328a83c672e8ed2919ee59f98c0a42bc042273d6
Domedriver <mlaman@gmail.com>
2019-03-28 11:47:54 -0400

    updating packages

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 var notes = getNotes()
 
-// console.log the return value of the function getNotes
-// just wanted to add another comment here
-// now I'm going to add one more comment
 console.log(notes)
+
+console.log(validator.isURL('https/mead.io'))
