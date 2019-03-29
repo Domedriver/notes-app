de2eca450903f7002a2723c6cd1c958d83fade30
Domedriver <mlaman@gmail.com>
2019-03-29 15:30:45 -0400

    added yargs

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-var notes = getNotes()
+// Customize yargs
+yargs.version('1.1.0')
 
-console.log(notes)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demand: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demand: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
 
-console.log(chalk.black.bgRed.bold.underline('Error'))
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note...')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Display list of all notes',
+    handler: function() {
+        console.log('Displaying list of all notes...')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note...')
+    }
+})
+
+yargs.parse()
 
 
