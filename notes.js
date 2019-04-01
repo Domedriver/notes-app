df9b64e4638b171a1536a3e9f6cc4d5ebd6b7238
Domedriver <mlaman@gmail.com>
2019-04-01 08:39:23 -0400

    added functionality for addNote and removeNote

--- notes.js
+++ notes.js
@@ -1,5 +1,53 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
 const getNotes = function() {
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function(title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title;
+    })    
+    if (duplicateNotes.length == 0) {
+        notes.push({title, body})
+        saveNotes(notes)        
+        console.log(chalk.bgGreen.black.bold('New note added!'))
+    } else {        
+        console.log(chalk.bold.bgRed('Note title taken'))
+    }    
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const filteredList = notes.filter(note => note.title !== title)
+    if (notes.length == filteredList.length) {
+        console.log(chalk.bold.bgRed('No note found!'))
+    } else {
+        console.log(chalk.bgGreen.black.bold('Note removed!'))
+        saveNotes(filteredList)
+    }
+    
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch(e) {
+        return []
+    }    
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
